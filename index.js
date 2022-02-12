5a4c4020134a1b84c7529dec3f44c56b7589d27a
Mehedi Hasan <mehedihasansimit@gmail.com>
2022-02-12 12:30:42 +0600

    using Multer  operations

--- index.js
+++ index.js
@@ -3,17 +3,62 @@ const app = express();
 const router = express.Router();
 const router2 = require('./routerHandler'); 
 const cookieParser = require('cookie-parser');
-
+const multer = require('multer');
 
 const PORT = 3000;
+const FILE_PATH = './uploads';
 const path = require('path');
-
 app.set('view engine','ejs');
 app.use(cookieParser());
-
 app.use(express.text());
-
 app.use('/home',router);
+
+const storage = multer.diskStorage({
+	destination : (req,file,cb)=>{
+		cb(null,FILE_PATH)
+	},
+	filename : (req,file,cb)=>{
+		const fileName_uniquecode = `${Date.now()}-${Math.round(Math.random()*1E9)}`;
+		const fileExt = path.extname(file.originalname);
+		const fileName = file.originalname
+								.replace(fileExt,'')
+								.split(' ')
+								.join('-')+'-' ; 
+		const totalPath = (fileName + fileName_uniquecode + fileExt).toLowerCase() ;
+		cb(null,totalPath)
+	}
+})
+
+const upload = multer({
+	dest : FILE_PATH,
+	fileFilter : (req,file,cb)=>{
+		if(file.mimetype  === 'image/png' ||
+			file.mimetype === 'image/jpg' ||
+			file.mimetype === 'image/jpeg'){
+				cb(null,true);
+			}
+			else if (file.mimetype == 'text/plain') {
+				cb(null,true);
+			}
+		else{
+			cb(new Error ('Only get .jpeg, .jpg and .png files '))
+		}
+	},
+	limits : {
+		fileSize : 1000000
+	},
+	storage 
+	// preservePath
+});
+
+const cpUpload = upload.fields([
+	{ name: 'avatar', maxCount: 3 },
+ 	{ name: 'texts', maxCount: 1 }
+]);
+
+app.post('/',cpUpload,(req,res,next)=>{
+	
+});
 
 router.use('/router',router2);
 
@@ -23,7 +68,13 @@ router.all('/name',(req,res,next)=>{
 })
 
 app.use((err,req,res,next)=>{
-	res.status(406).send(err.message);
+	if (err instanceof multer.MulterError){
+		res.status(500).send('Server Problems');
+	}
+	else{
+		res.status(406).send(err.message);
+	}
+	
 })
 
 app.listen(PORT, function(err){
